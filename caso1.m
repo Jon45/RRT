25ac26fc2b805d0a783d7ae1dad15625018a1047
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-10-10 18:30:25 +0200

    Añadidos ejercicios 2.1, 2.2, 2.3 y 2.4

--- caso1.m
+++ caso1.m
@@ -11,3 +11,24 @@ Histogram[RandomTable]
 lambda=50;
 RandomExpTable=Table[RandomExp[lambda],1000];
 Histogram[RandomExpTable]
+
+
+(*Ejercicio 2.1*)
+nmax = 5000;
+lambda = 50;
+mu = 100;
+(*Ejercicio 2.2*)
+InterArrivalsTime = Table[RandomExp[lambda],nmax];
+(*Ejercicio 2.3*)
+ServiceTime = Table[RandomExp[mu],nmax];
+(*Ejercicio 2.4*)
+AcumSeries[listInterArrivalsTime_]:= Module[{AcumVal=0},AcumVal+=#&/@listInterArrivalsTime];
+Arrivals = AcumSeries[InterArrivalsTime];
+ (*Arrivals = Accumulate[InterArrivalsTime];*)
+ (*& para que considere que es una funci\[OAcute]n*)
+ FifoSchedulling [arrivals_,service_]:=Module[{n,checkTime},n=1;checkTime=arrivals[[1]]; (If [checkTime >= #,checkTime+=service[[n++]],checkTime=#+service[[n++]]])&/@arrivals];
+ Departures = FifoSchedulling [InterArrivalsTime,ServiceTime];
+ Show[ListPlot[Arrivals[[1;;20]]],ListPlot[Departures[[1;;20]]]]
+
+
+
