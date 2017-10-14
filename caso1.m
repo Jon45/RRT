2fc7c9bdfd907e3f77f141d6a7b9e7728fa0b145
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-10-14 18:46:55 +0200

    Añadido cálculo de gráfica de número de usuarios

--- caso1.m
+++ caso1.m
@@ -16,7 +16,7 @@ Histogram[RandomExpTable]
 
 (*Ejercicio 4*)
 nmax = 5000;
-lambda = 10;
+lambda = 90;
 mu = 100;
 
 InterArrivalsTime = Table[RandomExp[lambda],nmax];
@@ -30,8 +30,21 @@ Arrivals = AcumSeries[InterArrivalsTime];
 
  FifoSchedulling [arrivals_,service_]:=Module[{n,checkTime},n=1;checkTime=arrivals[[1]]; (If [checkTime >= #,checkTime+=service[[n++]],checkTime=#+service[[n++]]])&/@arrivals];
  Departures = FifoSchedulling [Arrivals,ServiceTime];
-
+ 
  Manipulate[Show[ListPlot[Arrivals[[origin;;origin+width]],PlotStyle->Red],ListPlot[Departures[[origin;;origin+width]],PlotStyle->Blue],PlotRange->All],{origin,1,nmax-width,1},{width,1,50,1}]
+
+
+ (*UserStepStair*)
+ calculateNumberUsers [arrivals_,service_]:= Module [ {nUsers}, nUsers=0; 
+ ArrivalsList = Transpose[{Arrivals,Table[1,Length[Arrivals]]}];
+ ServiceList = Transpose[{Departures,Table[-1,Length[Departures]]}];
+ MergedList = Join[{{0,0}},ArrivalsList,ServiceList];
+ MergedList = SortBy[MergedList,#[[1]]];
+ ({#[[1]],nUsers+=#[[2]]})&/@MergedList
+ ];
+ UserStepStair=calculateNumberUsers[Arrivals,Departures];
+ maxLength = Length [UserStepStair];
+ Manipulate[ListPlot[UserStepStair[[origin;;origin+width]],InterpolationOrder -> 0,Joined -> True],{origin,1,nmax-width,1},{width,1,50,1}]
 
 
 
