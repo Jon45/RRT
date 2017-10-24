56cfae9d1b15a3e3973c7d9647d755aef9ba04e8
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-10-24 12:30:49 +0200

    Añadido ejercicio 7

--- caso1.m
+++ caso1.m
@@ -99,4 +99,81 @@ probabilidadesPasta=Pasta[Arrivals,UserStepStair,nmax];
 ListPlot[probabilidadesPasta,PlotRange -> Full]
 
 
+  (*Ejercicio 7*)
+  Needs["RandomData`"]
+  
+  nmax=1000;
+  lambda=10;
+  mu=100;
+  
+  InterArrivalsTime=Table[RandomExp[lambda],nmax];
+  
+  ServiceTime=Table[RandomExp[mu],nmax];
+  
+  AcumSeries[listInterArrivalsTime_]:=Module[{AcumVal=0},AcumVal+=#&/@listInterArrivalsTime]; 
+  Arrivals=AcumSeries[InterArrivalsTime];
+  
+  FifoSchedulling[arrivals_,service_]:=Module[{n,checkTime,checkTime2,departures,MergedList},n=1;checkTime=arrivals[[1]];checkTime2=arrivals[[1]];departures=(If[checkTime<=checkTime2,
+  If[checkTime>=#,checkTime+=service[[n++]],checkTime=#+service[[n++]]],If[checkTime2>=#,checkTime2+=service[[n++]],checkTime=#+service[[n++]]]
+  ])&/@arrivals; (*Similar a la de MM1, s\[OAcute]lo que ahora hay 2 checktimes. Se comprueba en cada iteraci\[OAcute]n cual es m\[AAcute]s peque\[NTilde]o y se utiliza ese checktime para hacer las mismas operaciones que en caso MM1. 
+  Se podr\[IAcute]an pasar 2 tiempos de servicio para poder simular colas con diferentes u. Intentar\[EAcute] hacerlo para m\[AAcute]s tarde. Tambi\[EAcute]n separar por colas para poder analizar el comportamiento de cada cola*)
+  MergedList=Transpose[{arrivals,departures}];
+  Transpose[SortBy[MergedList,Last]] (*Se han juntado las listas para ordenarlas conjuntamente, de manera que se pueda seguir asociando cada tiempo de salida a su tiempo de llegada correspondiente*)
+  ];
 
+  {Arrivals,Departures}=FifoSchedulling[Arrivals,ServiceTime];
+  Length[Departures]
+  
+  Manipulate[Show[ListPlot[Arrivals[[origin;;origin+width]],PlotStyle->Red],ListPlot[Departures[[origin;;origin+width]],PlotStyle->Blue],PlotRange->All],{origin,1,nmax-width,1},{width,1,50,1}]
+
+
+(*UserStepStair*)
+calculateNumberUsers[arrivals_,service_]:=Module[{nUsers},nUsers=0;
+ArrivalsList=Transpose[{Arrivals,Table[1,Length[Arrivals]]}];
+ServiceList=Transpose[{Departures,Table[-1,Length[Departures]]}];
+MergedList=Join[{{0,0}},ArrivalsList,ServiceList];
+MergedList=SortBy[MergedList,#[[1]]];
+({#[[1]],nUsers+=#[[2]]})&/@MergedList];
+UserStepStair=calculateNumberUsers[Arrivals,Departures];
+maxLength=Length[UserStepStair];
+Manipulate[ListPlot[UserStepStair[[origin;;origin+width]],InterpolationOrder->0,Joined->True],{origin,1,nmax-width,1},{width,1,50,1}]
+
+
+calculateStepStair[lista_]:=Module[{nUsers},nUsers=0;
+Join[{{0,0}},({#,nUsers+=1})&/@lista]];
+StepsArrivals=calculateStepStair[Arrivals];
+StepsDepartures=calculateStepStair[Departures];
+Manipulate[Show[ListPlot[StepsArrivals[[origin;;origin+width]],InterpolationOrder->0,Joined->True],ListPlot[StepsDepartures[[origin;;origin+width]],InterpolationOrder->0,Joined->True,PlotStyle->Red]],{origin,1,nmax-width,1},{width,1,50,1}]
+
+
+NPoints=100;
+Module[{InterArrivalsTime,ServiceTime,Arrivals,Departures,lambda,mu},
+mu=10;
+lambda=Range[1/NPoints,2*mu,1/NPoints];
+MeanWaitingTime=(InterArrivalsTime=Table[RandomExp[#],nmax];
+ServiceTime=Table[RandomExp[mu],nmax];
+Arrivals=AcumSeries[InterArrivalsTime];
+{Arrivals,Departures}=FifoSchedulling[Arrivals,ServiceTime];
+{#/(2*mu),Mean[Departures-Arrivals]})&/@lambda];
+ListPlot[MeanWaitingTime] (*Igual mejor separar por colas. Queda pendiente*)
+
+
+nmax=100;
+ProbabilidadesTeoricas[nmax_,p_]:=Module[{n,estado},n=Range[0,nmax,1];(If[#==0,(1-p)/(1+p),2*(1-p)/(1+p)*p^#])&/@n];
+pTeoricas=ProbabilidadesTeoricas[nmax,lambda/(2*mu)];
+ListPlot[pTeoricas,PlotRange->Full]
+
+ProbabilidadesSimuladas[UserStepStair_,nmax_]:=Module[{ocurrencias,n,p,i,acumval},ocurrencias=MapThread[({#2[[1]]-#1[[1]],#1[[2]]})&,{UserStepStair[[1;;Length[UserStepStair]-1]],UserStepStair[[2;;Length[UserStepStair]]]}];
+n=Range[0,nmax,1];
+p=(acumval=0;For[i=1,i<=Length[ocurrencias],i++,If[#==ocurrencias[[i,2]],acumval+=ocurrencias[[i,1]]]];acumval)&/@n;
+p/Total[p]];
+pSimuladas=ProbabilidadesSimuladas[UserStepStair,nmax];
+ListPlot[pSimuladas,PlotRange->Full]
+
+Pasta[Arrivals_,UserStepStair_,nmax_]:=Module[{position,n,cuentaPasta,probabilidadesPasta},cuentaPasta=(position=FirstPosition[Transpose[UserStepStair][[1]],#];Flatten[UserStepStair[[position]]][[2]]-1)&/@Arrivals;
+n=Range[0,nmax,1];
+probabilidadesPasta=(Count[cuentaPasta,#])&/@n;
+probabilidadesPasta/Total[probabilidadesPasta]];
+probabilidadesPasta=Pasta[Arrivals,UserStepStair,nmax];
+ListPlot[probabilidadesPasta,PlotRange->Full]
+(*Estar\[IAcute]a bien comparar con MM1 para rho m\[AAcute]s altas*)
