03b87ddce6c23314f81971d0f40312d9a2774954
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-10-24 11:02:19 +0200

    Añadidos comentarios para los ejercicios

--- caso1.m
+++ caso1.m
@@ -22,7 +22,7 @@ InterArrivalsTime = Table[RandomExp[lambda],nmax];
 
 ServiceTime = Table[RandomExp[mu],nmax];
 
-AcumSeries[listInterArrivalsTime_]:= Module[{AcumVal=0},AcumVal+=#&/@listInterArrivalsTime];
+AcumSeries[listInterArrivalsTime_]:= Module[{AcumVal=0},AcumVal+=#&/@listInterArrivalsTime]; (*Obtiene una lista con los valores acumulados: Por ejemplo,en la posici\[OAcute]n 3, lo que hab\[IAcute]a en la posici\[OAcute]n 1 + posici\[OAcute]n 2 + posici\[OAcute]n 3 de la lista de entrada,...*)
 Arrivals = AcumSeries[InterArrivalsTime];
  (*Arrivals = Accumulate[InterArrivalsTime];*)
  (*& para que considere que es una funci\[OAcute]n*)
@@ -35,19 +35,19 @@ Arrivals = AcumSeries[InterArrivalsTime];
 
  (*UserStepStair*)
  calculateNumberUsers [arrivals_,service_]:= Module [ {nUsers}, nUsers=0; 
- ArrivalsList = Transpose[{Arrivals,Table[1,Length[Arrivals]]}];
- ServiceList = Transpose[{Departures,Table[-1,Length[Departures]]}];
- MergedList = Join[{{0,0}},ArrivalsList,ServiceList];
- MergedList = SortBy[MergedList,#[[1]]];
- ({#[[1]],nUsers+=#[[2]]})&/@MergedList
+ ArrivalsList = Transpose[{Arrivals,Table[1,Length[Arrivals]]}]; (*Crea una lista de tuplas, un campo con el tiempo de llegada y otro con el valor 1*)
+ ServiceList = Transpose[{Departures,Table[-1,Length[Departures]]}]; (*Crea una lista de tuplas, un campo con el tiempo de salida y otro con el valor -1*)
+ MergedList = Join[{{0,0}},ArrivalsList,ServiceList]; (*A\[NTilde]ade el punto 0,0 de origen para que se visualize posteriormente*)
+ MergedList = SortBy[MergedList,#[[1]]]; (*Ordena la lista en funci\[OAcute]n del primero de los campos: Tiempo de llegada o tiempo de salida dependiendo del elemento*)
+ ({#[[1]],nUsers+=#[[2]]})&/@MergedList (*Devuelve una lista con tiempos y el n\[UAcute]mero de usuarios que le corresponde a cada tiempo. Va sumando los valores del segundo campo, -1 y 1, para determinar el n\[UAcute]mero de usuarios*)
  ];
  UserStepStair=calculateNumberUsers[Arrivals,Departures];
  maxLength = Length [UserStepStair];
- Manipulate[ListPlot[UserStepStair[[origin;;origin+width]],InterpolationOrder -> 0,Joined -> True],{origin,1,nmax-width,1},{width,1,50,1}] (*ListStepPlot tambi\[EAcute]n vale*)
+ Manipulate[ListPlot[UserStepStair[[origin;;origin+width]],InterpolationOrder -> 0,Joined -> True],{origin,1,nmax-width,1},{width,1,50,1}] (*ListStepPlot tambi\[EAcute]n vale. Interpolation order y joined para que te una los puntos en forma de escalera*)
 
 
  calculateStepStair [lista_]:= Module [ {nUsers}, nUsers=0;
- Join [{{0,0}},({#,nUsers+=1})&/@lista]
+ Join [{{0,0}},({#,nUsers+=1})&/@lista] (*A\[NTilde]ade el punto 0,0. El resto de puntos consisten en un valor de tiempos y un valor acumulado al que se va sumando 1 cada vez que se genera un nuevo punto*)
  ];
  StepsArrivals = calculateStepStair[Arrivals];
  StepsDepartures = calculateStepStair[Departures];
@@ -56,23 +56,23 @@ Arrivals = AcumSeries[InterArrivalsTime];
 
 (*Ejercicio 5*)
 NPoints=100;
-Module[{InterArrivalsTime,ServiceTime,Arrivals,Departures,lambda,mu},
-lambda=Range[1/NPoints,1,1/NPoints];
-mu=1;
+Module[{InterArrivalsTime,ServiceTime,Arrivals,Departures,lambda,mu}, (*Module para que no nos toque las variables que ya tenemos definidas*)
+lambda=Range[1/NPoints,1,1/NPoints]; (*Los valores de lambda para el experimento dependeran del n\[UAcute]mero de puntos*)
+mu=1; (*Normalizamos con esta mu para que lambda=rho*)
 MeanWaitingTime=(
 InterArrivalsTime = Table[RandomExp[#],nmax];
 ServiceTime = Table[RandomExp[mu],nmax];
 Arrivals = AcumSeries[InterArrivalsTime];
 Departures = FifoSchedulling [Arrivals,ServiceTime];
-{#,Mean[Departures-Arrivals]}
-)&/@lambda;
+{#,Mean[Departures-Arrivals]} (*Se acabara devolviendo una lista de puntos con lambda (o rho en este caso) y el tiempo medio de espera*)
+)&/@lambda; (*Calcula para los diferentes valores de lambda*)
 ]
 ListPlot[MeanWaitingTime]
 
 
 (*Ejercicio 6*)
 nmax=100;
-ProbabilidadesTeoricas[nmax_,p_]:= Module[{n},n=Range[0,nmax,1];((1-p)*p^n)&/@n];
+ProbabilidadesTeoricas[nmax_,p_]:= Module[{n},n=Range[0,nmax,1];((1-p)*p^n)&/@n]; (*F\[OAcute]rmula te\[OAcute]rica*)
 pTeoricas=ProbabilidadesTeoricas[nmax,lambda/mu];
 ListPlot[pTeoricas,PlotRange -> Full]
 
@@ -93,6 +93,3 @@ probabilidadesPasta/Total[probabilidadesPasta]
 ];
 probabilidadesPasta=Pasta[Arrivals,UserStepStair,nmax];
 ListPlot[probabilidadesPasta,PlotRange -> Full]
-
-
-
