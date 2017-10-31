9837b6cc7bac195084a79bb046a2647004aaf461
Jon Echebarria <jechebarria008@ikasle.ehu.eus>
2017-10-31 18:30:34 +0100

    Añadido cálculo de paquetes Stop & Wait, incluida gráfica

--- caso2.m
+++ caso2.m
@@ -18,6 +18,9 @@ nmax = 1000;
 lambda = 1;
 mu = 10;
 
+tp=0.5;
+tack=0.1;
+
 InterArrivalsTime = Table[RandomExp[lambda],nmax];
 
 ServiceTime = Table[RandomExp[mu],nmax];
@@ -36,7 +39,58 @@ sequenceNumbers = Table[n,{n,1,Length[injTimes],1}];
 lstPck = MapThread[({#1,9600*#2,#3,0,0})&,{injTimes,ServiceTime,sequenceNumbers}];
 
 
+SetIniParDraw[tp,tack];
 Manipulate [ Show [ DrawWin[origin,ww,8],Map[DrawPacketTx[#]&, SelectPacketInWin[lstPck]]],{origin,0,100-ww},{ww,1,50}]
 
 
+PacketArrivalsGenTimePeriod[lambda_,mu_,period_,ini_] := Module[{llegada,llegadas,len,n},
+len=ini + period;
+llegada = ini + RandomExp[lambda];
+n=1;
+llegadas={};
+While [llegada < len,
+AppendTo[llegadas,llegada];
+n++;
+llegada+=RandomExp[lambda];
+];
+llegadas
+];
 
+FifoPacketTxSW[lstArr_,p_]:= Module[{n,checkTime,paquetes,lista,numRepeticiones,paquetesIter},n=1;checkTime=lstArr[[1,1]]; paquetes={}; (lista=GetPacketRTxSW[#,p];paquetesIter = lista[[2]]; numRepeticiones=lista[[1]]; If [checkTime >= #[[1]],
+checkTime+=numRepeticiones*(#[[2]]/9600+tack+2*tp),
+checkTime=#[[1]]+numRepeticiones*(#[[2]]/9600+tack+2*tp)
+];
+n=numRepeticiones+1;
+paquetesIter = (n--;{checkTime-n*(#[[2]]/9600+tack+2*tp),#[[2]],#[[3]],#[[4]],#[[5]]})&/@paquetesIter;
+AppendTo[paquetes,paquetesIter];
+)&/@lstArr;
+Partition[Flatten[paquetes],5]
+];
+
+GetPacketRTxSW[pck_,p_]:= Module[{numRepet,paquetes},
+numRepet= Length[NestWhileList[#&,p,#>RandomReal[]&]];
+paquetes = Table[{pck[[1]],pck[[2]],pck[[3]],1,n},{n,1,numRepet,1}];
+paquetes[[numRepet,4]]=0;
+{numRepet,paquetes}
+];
+
+
+nmax=100;
+lambda=1;
+u=2;
+p=0.5;
+ti=1/u;
+a=(ti + 2*tp + tack)*u;
+
+InterArrivalsTime = Table[RandomExp[lambda],nmax];
+ServiceTime = Table[RandomExp[mu],nmax];
+
+Arrivals = AcumSeries[InterArrivalsTime];
+
+sequenceNumbers = Table[n,{n,1,Length[Arrivals],1}];
+
+paquetesLlegados = MapThread[({#1,9600*#2,#3,0,0})&,{Arrivals,ServiceTime,sequenceNumbers}];
+
+lstPck = FifoPacketTxSW[paquetesLlegados,p];
+SetIniParDraw[tp,tack];
+Manipulate [ Show [ DrawWin[origin,ww,8],Map[DrawPacketTx[#]&, SelectPacketInWin[lstPck]]],{origin,0,100-ww},{ww,0.1,50}]
